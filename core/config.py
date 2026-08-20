ecde5110ec5623092350070156b5120a1b1dd0fa
SALAUDDIN <mdsalauddinrony29@gmail.com>
2026-08-20 21:16:21 +0600

    work this config and security folder

---
+++ core/config.py
@@ -0,0 +1,26 @@
+def get_secret_key():
+    pass
+
+def get_api_key():
+    pass
+
+def is_production():
+    pass
+
+def get_allowed_hosts():
+    pass
+
+def get_cors_origins():
+    pass
+
+def database_url():
+    pass
+
+def get_redis_url():
+    pass
+
+def get_jwt_expiration():
+    pass
+
+def get_max_upload_size():
+    pass
---
+++ core/security.py
@@ -0,0 +1,28 @@
+def hash_password():
+    pass
+
+def verify_password():
+    pass
+
+def generate_token():
+    pass
+
+def verity_token():
+    pass
+
+def create_access_token():
+    pass
+
+def create_refresh_token():
+    pass
+
+def decode_token():
+    pass
+
+def encrypt_data():
+    pass
+
+def decrypt_data():
+    pass
+def rate_limit_check():
+    pass
